6ccf90db5996335e50c38810c532569690497eaa
sorgazb01 <sorgazb01@educarex.es>
2024-10-11 12:46:00 +0200

    ejercicios comunidades acabado

--- JavaScript/Capas/Manejo-DOM/js/script.js
+++ JavaScript/Capas/Manejo-DOM/js/script.js
@@ -11,18 +11,16 @@ function inicio(){
             }else{
                 alert("-->Has pulsado "+comunidad.textContent+" ,hijo del Norte")
             }
-        }
-        const listadoProvincias = comunidad.nextElementSibling
-        for(const provincia of listadoProvincias.children){
-            provincia.onclick = () =>{
-                let numHijo
-                for (let index = 0; index < listadoProvincias.children.length; index++) {
-                    if(listadoProvincias.children[index] == provincia){
-                        numHijo = index
+            const listadoProvincias = comunidad.nextElementSibling
+            for(const provincia of listadoProvincias.children){
+                    let numHijo
+                    for (let index = 0; index < listadoProvincias.children.length; index++) {
+                        if(listadoProvincias.children[index] == provincia){
+                            numHijo = index
+                        }
+                        
                     }
-                    
-                }
-                alert("Hijo   "+(numHijo+1)+" : "+provincia.textContent)
+                    alert("Hijo   "+(numHijo+1)+" : "+provincia.textContent)
             }
         }
     }
