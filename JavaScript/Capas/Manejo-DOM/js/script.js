fafd542048d2cf1f0e2c021073fc8cff0c6a2aea
sorgazb01 <sorgazb01@educarex.es>
2024-10-13 19:26:53 +0200

    Solicion Ejercicio DOM 11octubre

---
+++ JavaScript/Capas/Manejo-DOM/js/script.js
@@ -0,0 +1,34 @@
+window.onload = inicio
+
+const listaComunidades = document.querySelectorAll("h2") 
+
+function inicio(){
+
+    for (const comunidad of listaComunidades) {
+        comunidad.onclick = ()=>{
+            if(comunidad.textContent == 'Andalucía' || comunidad.textContent == "Canarias"){
+                alert("-->Has pulsado "+comunidad.textContent+" ,hijo del Sur")
+            }else{
+                alert("-->Has pulsado "+comunidad.textContent+" ,hijo del Norte")
+            }
+        }
+        const listadoProvincias = comunidad.nextElementSibling
+        for(const provincia of listadoProvincias.children){
+            provincia.onclick = () =>{
+                let numHijo
+                for (let index = 0; index < listadoProvincias.children.length; index++) {
+                    if(listadoProvincias.children[index] == provincia){
+                        numHijo = index
+                    }
+                    
+                }
+                alert("Hijo "+(numHijo+1)+" : "+provincia.textContent)
+            }
+        }
+    }
+} 
+
+
+
+
+
