08d6a610b69e96d6773e8a342137732933c2233f
sorgazb01 <sorgazb01@educarex.es>
2024-10-17 20:41:13 +0200

    Ejercicio 4 acabado

--- JavaScript/APIS/Tarea-4-16-Otc/js/script1.js
+++ JavaScript/APIS/Tarea-4-16-Otc/js/script1.js
@@ -1,23 +1,37 @@
 window.addEventListener("load", inicio);
 
+// URL de los datos a trabajar
 const urlAPI = "http://moralo.atwebpages.com/ajaxListar/getTodoPersonal.php";
 
-const contenedorPadre = document.querySelectorAll(".col-md-4");
+// Como existen dos contenedores con la clase ".row gx-4 gx-lg-5" del Contenedor Padre de los Cards
+// los recuperamos con un querySelectorAll
+const contenedoresRow = document.querySelectorAll(".row.gx-4.gx-lg-5");
+
+// Y recuperamos el Contenedor de los Cards
+const contenedorCards = obtenerContenedorCards()
+
 
 function inicio() {
   limpiarCaja();
 
   fetch(urlAPI)
     .then((response) => response.json())
-    .then((data) => mostrarDatos(data));
-  // .finally(() => console.log("Terminado."))
-  // .catch((error) => alert(error))
+    .then((data) => mostrarDatos(data))
+    .finally(() => console.log("Terminado."))
+    .catch((error) => alert(error))
 
+  
   function mostrarDatos(datos) {
       datos.forEach((dato) => {
+        //Creamos las columnas y Cards
+        
+        const columna = document.createElement("div")
+        columna.className = "col-md-4 mb-5"
+        contenedorCards.appendChild(columna);
+
         const tarjeta = document.createElement("div");
         tarjeta.className = "card";
-        contenedorPadre[0].appendChild(tarjeta);
+        columna.appendChild(tarjeta);
 
         const imagenWEB = document.createElement("img");
         imagenWEB.setAttribute("src", dato.imagen);
@@ -30,21 +44,28 @@ function inicio() {
         const titulo = document.createElement("h2");
         titulo.className = "card-title";
         titulo.textContent = dato.nombre;
+        cardBody.appendChild(titulo);
 
         const subtitulo = document.createElement("p");
         subtitulo.className = "card-text";
         subtitulo.textContent = dato.cargo;
-
-        cardBody.appendChild(titulo);
         cardBody.appendChild(subtitulo);
       });
   }
 }
 
-function limpiarCaja() {
-  contenedorPadre.forEach((bloque) => {
-    while (bloque.hasChildNodes()) {
-      bloque.removeChild(bloque.firstChild);
+// Como el contenedor de los Cards contiene 3 elementos recuperamos ese del array
+function obtenerContenedorCards(){
+  for (const contenedor of contenedoresRow) {
+    if(contenedor.children.length == 3){
+      return contenedor
     }
-  });
+  }
+}
+
+// Funcion para limpiar el contenedor del HTML
+function limpiarCaja() {
+    while (contenedorCards.hasChildNodes()) {
+      contenedorCards.removeChild(contenedorCards.firstChild);
+    }
 }
