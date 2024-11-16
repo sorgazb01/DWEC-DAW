8db7226a81802362dbd4e0fd041c3ea397e194f1
sorgazb01 <sorgazb01@educarex.es>
2024-11-16 22:30:01 +0100

    ejercicio conversion moneda acabado

--- Angular/Conversor-Monetario/src/app/app.component.ts
+++ Angular/Conversor-Monetario/src/app/app.component.ts
@@ -16,8 +16,16 @@ export class AppComponent {
     this.yenes = 0
   }
 
-  conversion(){
-    this.dolares = this.euros*0.95
-    this.yenes = this.euros*164
+  conversion(moneda:string){
+    if(moneda == 'euros'){
+      this.dolares = this.euros * 0.95
+      this.yenes = this.euros * 162.76
+    }else if(moneda == 'dolares'){
+      this.euros = this.dolares * 1.05
+      this.yenes = this.dolares * 154.33
+    }else if(moneda == 'yenes'){
+      this.euros = this.yenes * 0.0061
+      this.dolares = this.yenes * 0.0065
+    }
   }
 }
